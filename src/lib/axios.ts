d6aa98b6f70304234bae05df9bbf4770d950e480
Damianpiazz <damipiazza333@gmail.com>
2026-04-21 00:38:47 -0300

    ➕ ADD(http-client): implementa instancia basica de axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,1 +1,10 @@
-//
+import Axios from "axios";
+
+const httpClient = Axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default httpClient;
